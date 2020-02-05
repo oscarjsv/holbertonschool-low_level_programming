1edeeee7ba6b6b6a4e92701293bf04ef22500496
oscar sanchez <gumball_junior@hotmail.com>
2020-02-05 21:11:33 +0000

    helloputs

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-<stdio.h>
+#<stdio.h>
 
      main - Entry point
     
@@ -6,6 +6,6 @@
     
 int main(void)
 {
-puts ("\"Programming is like building a multilingual puzzle);
+puts ("\"Programming is like building a multilingual puzzle");
 return (0);
 }
