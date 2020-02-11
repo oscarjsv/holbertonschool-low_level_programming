791075503c82724ee4469788f2205723011d10ca
oscar sanchez <gumball_junior@hotmail.com>
2020-02-11 13:55:29 +0000

    thealphabetx10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,12 @@
 #include "holberton.h"
-void print_alphabet_x10(void){
+void print_alphabet_x10(void)
+{
 int i;
 char j;
-for (i = 0; i <= 10; i++){
-for (j = 'a'; j <= 'z'; j++){
+for (i = 0; i <= 10; i++)
+{
+for (j = 'a'; j <= 'z'; j++)
 _putchar(j);
-}
 _putchar('\n');
 }
 }
