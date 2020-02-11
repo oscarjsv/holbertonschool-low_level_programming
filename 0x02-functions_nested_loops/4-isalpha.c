ffb8a8bc6faeb151c0752f5f58441557782bffcf
oscar sanchez <gumball_junior@hotmail.com>
2020-02-11 18:34:05 +0000

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * _isalpha - printf file an letter
+ * @c: An ascii char to compare
+ * return: return numb 1 numb depending of the condition
+ */
+int _isalpha(int c)
+{
+if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+{
+return (1);
+}
+return (0);
+}
