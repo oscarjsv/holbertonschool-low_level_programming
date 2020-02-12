60c2d29ec379c8c4c75ed68b8661b2cb5b93aabf
oscar sanchez <gumball_junior@hotmail.com>
2020-02-12 00:27:52 +0000

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * add - check the code for Holberton School students.
+ *@a: An ascii char to compare
+ *@b: An ascii char to compare
+ * Return: Always 0.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
