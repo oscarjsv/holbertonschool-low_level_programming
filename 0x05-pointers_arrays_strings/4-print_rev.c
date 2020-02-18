c9aeedeaa5534f398023e0ea7160b3cbc0358194
oscarjsv <gumball_junior@hotmail.com>
2020-02-18 11:05:50 -0500

    4print

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,14 +8,14 @@ void print_rev(char *s)
 {
 	int b;
 
-	for (b = 0; *s != '0'; b++)
+	for (b = 0; s[b] != '\0'; b++)
 	{
 	}
-b--;
+	b++;
 
-	for (; 0 >= '\0'; b--)
+	for (; b >= 0; b--)
 	{
-		_putchar(b);
+		_putchar(s[b]);
 	}
 	_putchar('\n');
 }
