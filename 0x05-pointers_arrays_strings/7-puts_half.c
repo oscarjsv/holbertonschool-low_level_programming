ce65d0f0615f96472e61a50a7449706b43abb17e
oscarjsv <gumball_junior@hotmail.com>
2020-02-18 17:17:47 -0500

    pointerarraystring

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * puts_half - Print a square
+ *@str :  Size of the square
+ *
+ */
+void puts_half(char *str)
+{
+	int a;
+	int n;
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+	}
+
+	if (a % 2 == 0)
+	{
+		n = a / 2;
+	}
+	else
+	{
+		n = a - 1 / 2;
+	}
+
+	for (; str[n] != '\0'; n++)
+	{
+		_putchar(str[n]);
+	}
+	_putchar('\n');
+}
