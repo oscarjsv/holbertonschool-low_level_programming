8ee8e0e5a233efb0a167977aed4ebcfc4465ff96
oscarjsv <gumball_junior@hotmail.com>
2020-02-26 14:54:19 -0500

    ejercicios

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _print_rev_recursion - Prints a string in reverse mode.
+ * @s: String
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+	else
+	{
+
+		_putchar('\n');
+	}
+}
