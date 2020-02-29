775cfe3783e535914472418723ec07a55e018006
oscarjsv <gumball_junior@hotmail.com>
2020-02-29 15:19:06 -0500

    myname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,7 +5,7 @@
  * @argv: array of strings
  * Return: 0
  */
-int main(int __attribute__((__unused__))argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	printf("%s\n", (argv[0]));
 	return (0);
