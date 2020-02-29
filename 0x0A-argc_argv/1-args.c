0b6c6784ce0c19a8ac4ea0aac0b2c53f3fa95bee
oscarjsv <gumball_junior@hotmail.com>
2020-02-29 16:03:09 -0500

    1args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - function
+ * @argc: number of arguments
+ * @argv: array of strings
+ * Return: 0
+ */
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("argc = %d\n", argc);
+	return (0);
+}
