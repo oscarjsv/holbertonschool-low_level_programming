67909943ead914e2632a89bcaa30dc954ce60937
oscarjsv <gumball_junior@hotmail.com>
2020-02-29 18:13:04 -0500

    3multi

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,21 +8,20 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, sum;
-	sum = 1;
+	int i, sum = 1;
 
-	if(argc < 2)
-	{
-		printf("Error");
-		return (1);
-	}
-	else
+	if (argc > 2)
 	{
 		for (i = 1; i < argc; i++)
 		{
 			sum = sum * atoi(argv[i]);
 
 		}
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
 	}
 	printf("%d\n", sum);
 	return (0);
