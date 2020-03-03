9e2bfadebb51cb4fe76f4d8369ae1ede29814628
oscarjsv <gumball_junior@hotmail.com>
2020-03-03 13:29:57 -0500

    1point

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,6 +17,10 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	}
 	p = malloc(sizeof(char) * size);
+	if (p == NULL)
+	{
+		return (NULL);
+	}
 
 	for (i = 0; i <= size; i++)
 	{
