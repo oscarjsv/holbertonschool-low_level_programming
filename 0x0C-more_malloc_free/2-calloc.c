6e11d226ecfeaff9bbb36acc047f2c924c2edc5e
oscarjsv <gumball_junior@hotmail.com>
2020-03-05 17:15:27 -0500

    2calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -21,13 +21,15 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 	}
 	C = malloc(nmemb * size);
+
 	if (C != NULL)
 	{
-		for (i = 0; i < nmemb * size; i++)
+		for (i = 0; i < (nmemb * size); i++)
 		{
 			*(C + i) = 0;
-			return (C);
+
 		}
+		return (C);
 	}
 	if(C == NULL)
 	{
