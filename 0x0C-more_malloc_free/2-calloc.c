e2b48f6baa83e95671aacd37e05aef9387866451
oscarjsv <gumball_junior@hotmail.com>
2020-03-05 17:10:13 -0500

    2calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates the requested memory
+ * @nmemb: size of the string
+ * @size: number of bytes
+ * Return: requested memory or NULL
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *C;
+	unsigned int i;
+
+	if (nmemb == 0)
+	{
+		return (NULL);
+	}
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	C = malloc(nmemb * size);
+	if (C != NULL)
+	{
+		for (i = 0; i < nmemb * size; i++)
+		{
+			*(C + i) = 0;
+			return (C);
+		}
+	}
+	return (NULL);
+}
