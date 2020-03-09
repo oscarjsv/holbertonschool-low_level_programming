4a0a39ac6b532697c1050ade94312f8a040f5097
oscarjsv <gumball_junior@hotmail.com>
2020-03-09 16:22:18 -0500

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structura
+ * @name: pointer
+ * @age: float
+ * @owner: pinter
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
