9828114ef72a95cf2eeea99c16dd5b169af37c35
oscarjsv <gumball_junior@hotmail.com>
2020-03-10 16:17:02 -0500

    5step

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,16 +1,16 @@
 #include "dog.h"
 #include <stdlib.h>
-
 /**
- * free_dog - Frees dogs
- * @d: Dog to free
- **/
+ * free_dog - frees memory used on structure
+ * @d: pointer to structure
+ * Return: void
+ */
 void free_dog(dog_t *d)
 {
 	if (d == NULL)
 		return;
 
 	free(d->name);
-	free(d->owner);
 	free(d);
+	free(d->owner);
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,19 +1,26 @@
-#ifndef DOG_H
-#define DOG_H
-
+#ifndef _dog_H_
+#define _dog_H_
 /**
- * struct dog - structura
- * @name: pointer
- * @age: float
- * @owner: pinter
- */
-typedef struct dog
+  * struct dog - dog's information
+  * @name: dog's name
+  * @age: dog's age
+  * @owner: dog's owner
+  *
+  * Description: dog's information
+  */
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+/**
+  * dog_t - typedef for struct
+  */
+typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
 #endif
