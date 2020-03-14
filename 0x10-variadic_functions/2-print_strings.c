ad25da741a124c05c893f2454fed5fcda5de9dfc
oscarjsv <gumball_junior@hotmail.com>
2020-03-14 14:39:12 -0500

    2print

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -26,7 +26,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		{
 			printf("%s", rec);
 		}
-		if (i != (n-1) && separator != NULL)
+		if (i != (n - 1) && separator != NULL)
 		{
 			printf("%s", separator);
 		}
