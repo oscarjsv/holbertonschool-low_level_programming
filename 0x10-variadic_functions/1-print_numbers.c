b9a2cf85e0c2d1c59be5e639d3d46fc09c6f012c
oscarjsv <gumball_junior@hotmail.com>
2020-03-14 14:10:11 -0500

    1print

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,6 +1,7 @@
- #include <stdio.h>
-#include <stdarg.h>
 #include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
 /**
  * print_numbers - prints numbers, followed by a new line.
  * @rec: a pointer to a string.
