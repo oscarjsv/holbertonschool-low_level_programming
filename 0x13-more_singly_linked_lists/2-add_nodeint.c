cf3a09cf67ab24f67c6c40d63a1ae8edca496965
oscarjsv <gumball_junior@hotmail.com>
2020-03-24 09:36:57 -0500

    checkcorrection

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -17,7 +17,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 		return (NULL);
 	}
 	newnodo->n = n;
-	newnodo->next = (*head);
-	(*head) = newnodo;
+	newnodo->next = *head;
+	*head = newnodo;
 	return (NULL);
 }
