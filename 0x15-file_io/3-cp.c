32a9f3f90bf134abf0da220f028ab13a29f6919f
oscarjsv <gumball_junior@hotmail.com>
2020-03-31 22:59:27 -0500

    3cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,10 +1,10 @@
 #include "holberton.h"
 /**
+ *main - entry point.
+ *@argc: number of argument
+ *@argv: character value an array of pointers to the strings.
  *
- *main - wherever
- *@argc : number of arguments
- *@argv : caracter value
- *Return : return 0 and exit
+ *Return: 0 if success.
  */
 int main(int argc, char **argv)
 {
@@ -12,16 +12,14 @@ int main(int argc, char **argv)
 	int fr, f1, f2;
 
 	if (argc != 3)
-	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-	}
 	f1 = open(argv[1], O_RDONLY);
 	if (f1 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	f2 = open(argv[2], O_CREAT | O_RDONLY | O_TRUNC, 0664);
+	f2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (f2 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
@@ -43,14 +41,8 @@ int main(int argc, char **argv)
 		exit(98);
 	}
 	if (close(f2))
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f2);
-		exit(100);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f2), exit(100);
 	if (close(f1))
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f1);
-		exit(100);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f1), exit(100);
 	return (0);
 }
