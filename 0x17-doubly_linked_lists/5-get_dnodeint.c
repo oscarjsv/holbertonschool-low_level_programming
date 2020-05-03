1e72edd9f6eab7e2027bbb283349a1c21dd446cc
oscarjsv <gumball_junior@hotmail.com>
2020-05-03 18:17:13 -0500

    5add

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 /**
- *
- *
- *
- *
+ * get_dnodeint_at_index - returns the nth node
+ * @head: first node
+ * @index: node that I want
+ * Return: nth node
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
