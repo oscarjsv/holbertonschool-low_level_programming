57bd1324d8e6cf6e6d639621f58d0eabe1012dee
oscarjsv <gumball_junior@hotmail.com>
2020-05-29 11:52:41 -0500

    0add

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,7 +9,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *ht = NULL;
 
 	ht = malloc(sizeof(hash_table_t));
-	if (!ht)
+	if (ht == NULL)
 		return (NULL);
 
 	ht->array = malloc(sizeof(hash_node_t *) * size);
