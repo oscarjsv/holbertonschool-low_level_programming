cbc5742f916c11431bf8cd519633602c59103781
oscarjsv <gumball_junior@hotmail.com>
2020-05-29 18:55:46 -0500

    6aad

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a table
+ * @ht: pounter to the table
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *ptr;
+
+	if (ht == NULL || ht->array == NULL || ht->size == 0)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		while (ht->array[i])
+		{
+			ptr = ht->array[i]->next;
+			free(ht->array[i]->key);
+			free(ht->array[i]->value);
+			free(ht->array[i]);
+			ht->array[i] = ptr;
+		}
+	}
+	free(ht->array);
+	ht->array = NULL;
+	ht->size = 0;
+	free(ht);
+}
